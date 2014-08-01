d327b430904e40dfcebbdcb7e2ad30ea6d946da2
Jamie Sahote <jsahote@mit.edu>
2014-08-01 11:27:16 -0400

    added new aukernals and bcs

---
+++ src/bcs/CoolantNeumannBC.C
@@ -0,0 +1,41 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+// 
+
+#include "CoolantNeumannBC.h"
+
+template<>
+InputParameters validParams<CoolantNeumannBC>()
+{
+  InputParameters params = validParams<IntegratedBC>();
+  params.addRequiredParam<Real>("T_coolant", "The temperature of the bulk coolant");
+  params.addRequiredCoupledVar("heat_transfer_coefficient", "The heat transfer coefficient of the bulk coolant");
+
+  return params;
+}
+
+CoolantNeumannBC::CoolantNeumannBC(const std::string & name, InputParameters parameters)
+ :IntegratedBC(name, parameters),
+  _T_coolant(getParam<Real>("T_coolant")),
+  _heat_transfer_coefficient(coupledValue("heat_transfer_coefficient"))
+  
+
+{}
+
+Real
+CoolantNeumannBC::computeQpResidual()
+{
+  return _heat_transfer_coefficient[_qp] * _test[_i][_qp] * (_u[_qp] - _T_coolant);
+}
