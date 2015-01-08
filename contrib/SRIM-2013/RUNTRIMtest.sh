3171c66cfbc2abad57d4426efb5f1ed0d25e970a
22.033 F14 Student <ledoux@mit.edu>
2015-01-08 14:52:19 -0500

    debugging. Seems to be working with this update.

--- contrib/SRIM-2013/RUNTRIMtest.sh
+++ contrib/SRIM-2013/RUNTRIMtest.sh
@@ -1,20 +1,20 @@
-#!/bin/bash                                                                                      
+#!/bin/bash                                                                                     
 
 
-for i in {1..2..1}  ## energies
+for energies in {1..2}  ## energies
 do
-        for j in {1..2..1}  ##sample thickness
-	do
-	    for k in {1..2..1} ##coolant thickness
-            do
-                        sed 's/<BEAMENERGY>/"$i"/'  TRIM-Script-sample-SS316.IN > TRIM1.IN
-                        sed 's/<SAMPTHICK>/"$j"/' TRIM1.IN > TRIM2.IN
-                        sed 's/<COOLTHICK>/"$k"/' TRIM2.IN > TRIM.IN
-                        wine TRIM.exe
-			touch vacancy_files/VAC-SS316COOLANT-CTHICK-"$k"-SAMPLETHICK-"$j"-E-"$i".txt
-                        cp VACANCY.txt vacancy_files/VAC-SS316COOLANT-CTHICK-"$k"-SAMPLETHICK-"$j"-E-"$i".txt
-			touch range_files/RANGE-SS316COOLANT-CTHICK-"$k"-SAMPLETHICK-"$j"-E-"$i".txt
-                        cp RANGE.txt range_files/RANGE-SS316COOLANT-CTHICK-"$k"-SAMPLETHICK-"$j"-E-"$i".txt
-                done
-        done
+    for j in {1..2}  ##sample thickness
+    do
+	for k in {1..2} ##coolant thickness
+        do
+	    sed "s/<BEAMENERGY>/$energies/"  TRIM-Script-sample-SS316.IN > TRIM1.IN
+            sed "s/<SAMPTHICK>/$j/" TRIM1.IN > TRIM2.IN
+            sed "s/<COOLTHICK>/$k/" TRIM2.IN > TRIM.IN
+            wine TRIM.exe
+	    touch vacancy_files/VAC-SS316COOLANT-CTHICK-"$k"-SAMPLETHICK-"$j"-E-"$energies".txt
+            cp VACANCY.txt vacancy_files/VAC-SS316COOLANT-CTHICK-"$k"-SAMPLETHICK-"$j"-E-"$energies".txt
+	    touch range_files/RANGE-SS316COOLANT-CTHICK-"$k"-SAMPLETHICK-"$j"-E-"$energies".txt
+            cp RANGE.txt range_files/RANGE-SS316COOLANT-CTHICK-"$k"-SAMPLETHICK-"$j"-E-"$energies".txt
+	done
+    done
 done
